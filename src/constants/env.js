ba45c2f6083cc29eafa9f9bbbb8ce9418172a17a
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-05-12 16:39:30 +0200

    Update environment variables keys.

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,9 +1,9 @@
 import 'dotenv/config.js';
 
 const REQUIRED_ENV = [
-    "REACT_APP_NOMISWAP_ROUTER_ADDRESS",
-    "REACT_APP_NOMISWAP_TOKEN_MEM_ADDRESS",
-    "REACT_APP_NOMISWAP_TOKEN_NMO_ADDRESS"
+    "REACT_APP_NOMOSWAP_ROUTER_ADDRESS",
+    "REACT_APP_NOMOSWAP_TOKEN_NMO_ADDRESS",
+    "REACT_APP_NOMOSWAP_TOKEN_MEM_ADDRESS"
 ];
 
 const missingVars = REQUIRED_ENV.filter(key => !process.env[key]);
@@ -13,9 +13,9 @@ if (missingVars.length > 0) {
 }
 
 const config = {
-    routerAddress: process.env.REACT_APP_NOMISWAP_ROUTER_ADDRESS,
-    tokenMemAddress: process.env.REACT_APP_NOMISWAP_TOKEN_MEM_ADDRESS,
-    tokenNmoAddress: process.env.REACT_APP_NOMISWAP_TOKEN_NMO_ADDRESS
+    routerAddress: process.env.REACT_APP_NOMOSWAP_ROUTER_ADDRESS,
+    tokenNmoAddress: process.env.REACT_APP_NOMOSWAP_TOKEN_NMO_ADDRESS,
+    tokenMemAddress: process.env.REACT_APP_NOMOSWAP_TOKEN_MEM_ADDRESS
 };
 
 export default config;
